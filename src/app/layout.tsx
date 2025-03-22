a29a3ebf533a1452547311dc3b8a04cd556c7d93
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2025-03-22 13:59:16 -0300

    feat: adiciona footer nas páginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,11 +28,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} container mx-auto antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} container mx-auto flex flex-col p-6 antialiased`}
       >
         <Header />
 
-        {children}
+        <main>{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-auto text-center">
+      <p>PROJETO PRÁTICO IMPLEMENTAÇÃO FRONT-END</p>
+
+      <p>Feito por Jefferson Vieira da Silva</p>
+    </footer>
+  );
+}
