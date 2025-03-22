3d27c098d78bce88223640e57cca01780aa7c513
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2025-03-22 13:39:43 -0300

    feat: adiciona header nas páginas

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="p-6">
+      <h1 className="text-center text-2xl font-semibold">
+        Pessoas <span className="font-extrabold">Desaparecidas</span>
+      </h1>
+    </header>
+  );
+}
