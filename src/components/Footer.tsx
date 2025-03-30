953acd1ab485c08f22c76c6017a4e91d20efdac6
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2025-03-30 11:51:19 -0300

    feat: atualiza header e footer
    Ajustes visuais no header e footer para armonizar as página.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
+import { Card, CardContent } from '@/components/ui/card';
+
 export default function Footer() {
   return (
-    <footer className="mt-auto text-center">
-      <p>PROJETO PRÁTICO IMPLEMENTAÇÃO FRONT-END</p>
+    <footer className="mt-auto">
+      <Card className="rounded-none">
+        <CardContent className="space-y-1 text-center">
+          <p>PROJETO PRÁTICO IMPLEMENTAÇÃO FRONT-END</p>
 
-      <p>Feito por Jefferson Vieira da Silva</p>
+          <p>Feito por Jefferson Vieira da Silva</p>
+        </CardContent>
+      </Card>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="p-6">
-      <h1 className="text-center text-2xl font-semibold">
+      <h1 className="text-center text-4xl font-semibold">
         Pessoas <span className="font-extrabold">Desaparecidas</span>
       </h1>
     </header>
